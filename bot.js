9f9dbf1495c0911d150029cdb2d2a403ff23d870
Odd Anfinn Flakk <oddflakk13@hotmail.com>
2022-05-06 15:48:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -43,7 +43,7 @@ client.on('interactionCreate', async interaction => {
   });*/
 
 let job = new cron.CronJob('50 15 * * *', () => {
-  client.send("<@170242031342321675>");
+  client.channels.cache.get(channel).send("<@170242031342321675>")
   client.channels.cache.get(channel).send("https://i.imgur.com/bnjjOzL.png")
   })
 
