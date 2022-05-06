7e74d0c4d48270c9895826f85e32355d84f7b713
Odd Anfinn Flakk <oddflakk13@hotmail.com>
2022-05-06 05:11:22 +0200

    Added 2100 Event

--- bot.js
+++ bot.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
+const cron = require("cron");
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+let channel = "218739376413212674";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -13,5 +16,11 @@ client.on('interactionCreate', async interaction => {
     await interaction.reply("https://i.imgur.com/bnjjOzL.png");
   }
 });
+
+let job = new cron.CronJob('00 21 * * *', () => {
+  client.channels.cache.get(channel).send("https://i.imgur.com/bnjjOzL.png")
+  })
+
+job.start()
 
 client.login(process.env.TOKEN);
